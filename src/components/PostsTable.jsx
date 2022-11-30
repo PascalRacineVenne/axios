af73ea99d7ddb20dc1611992e8b3ea5c5d48b639
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-11-30 15:43:55 -0500

    changed style on posts table

--- src/components/PostsTable.jsx
+++ src/components/PostsTable.jsx
@@ -3,6 +3,7 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
+import Container from '@mui/material/Container';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
@@ -32,38 +33,44 @@ export default function DenseTable() {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size='small' aria-label='a dense table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell align='right'>Body</TableCell>
-            <TableCell align='right'>User Id&nbsp;(i)</TableCell>
-            <TableCell>Edit</TableCell>
-            <TableCell>Remove</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {posts.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component='th' scope='row'>
-                <p onClick={() => navigate(`/posts/${row.id}`)}>{row.title}</p>
-              </TableCell>
-              <TableCell align='right'>{row.body}</TableCell>
-              <TableCell align='center'>{row.userId}</TableCell>
-              <TableCell align='right'>
-                <EditOutlinedIcon onClick={() => navigate(`/edit/${row.id}`)} />
-              </TableCell>
-              <TableCell align='right' onClick={() => handleDelete(row.id)}>
-                <DeleteOutlinedIcon />
-              </TableCell>
+    <Container sx={{ margin: 5 }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size='small' aria-label='a dense table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>Body</TableCell>
+              <TableCell align='right'>User Id&nbsp;(i)</TableCell>
+              <TableCell>Edit</TableCell>
+              <TableCell>Remove</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {posts.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component='th' scope='row'>
+                  <p onClick={() => navigate(`/posts/${row.id}`)}>
+                    {row.title}
+                  </p>
+                </TableCell>
+                <TableCell>{row.body}</TableCell>
+                <TableCell align='center'>{row.userId}</TableCell>
+                <TableCell align='right'>
+                  <EditOutlinedIcon
+                    onClick={() => navigate(`/edit/${row.id}`)}
+                  />
+                </TableCell>
+                <TableCell align='right' onClick={() => handleDelete(row.id)}>
+                  <DeleteOutlinedIcon />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
