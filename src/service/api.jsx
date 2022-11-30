9844d735daf3f4886bc66fa0fffc445daaccdf9c
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-11-30 15:40:51 -0500

    nothing is actually working with the json server except Get requests

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
-
 const baseURL = 'https://jsonplaceholder.typicode.com/posts';
 
 export const getAllPosts = async (id) => {
@@ -11,20 +9,15 @@ export const getAllPosts = async (id) => {
 
 export const addPost = async (post) => {
   try {
-    const response = await axios.post(baseURL, post);
-    console.log(response);
+    return await axios.post(baseURL, post);
   } catch (error) {
     console.error(error);
   }
 };
 
 export const updatePost = async (id, updatedPost) => {
-  console.log('before TRY');
-  console.log(updatedPost);
   try {
-    console.log('In try');
-    const response = await axios.put(`${baseURL}/${id}`, updatedPost);
-    console.log(response);
+    return await axios.put(`${baseURL}/${id}`, updatedPost);
   } catch (error) {
     console.log(error.message);
   }
@@ -33,14 +26,3 @@ export const updatePost = async (id, updatedPost) => {
 export const deletePost = async (id) => {
   return await axios.delete(`${baseURL}/${id}`);
 };
-
-// const post = {
-//   title: 'foo',
-//   body: 'bar',
-//   userId: 1,
-// };
-// console.log('post was declared');
-// addPost(post);
-// console.log('function was called');
-// getAllPosts();
-// console.log('Get all function was called');
